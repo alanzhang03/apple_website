38a1b390c3e3399f4375b45ed4674f08237a505b
alanzhang03 <alan.s.zhang@gmail.com>
2024-07-31 10:00:25 -0400

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+
 const Hero = () => {
     return (
         <section className="w-full nav-height bg-black relative">
-            <div className="h-5/6">
+            <div className="h-5/6 w-full flex-center flex-col">
+                <p className="hero-title">Iphone 15 Pro</p>
             
             
             </div>
