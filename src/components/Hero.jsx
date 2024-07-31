348600c1f15ac2efa955bae68574ab3e19bebfe3
alanzhang03 <alan.s.zhang@gmail.com>
2024-07-31 09:57:23 -0400

    navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Hero = () => {
-	return <div>Hero</div>;
+    return (
+        <section className="w-full nav-height bg-black relative">
+            <div className="h-5/6">
+            
+            
+            </div>
+        </section>
+    
+    )
 };
 
 export default Hero;
